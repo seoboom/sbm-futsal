96c16e477d29a56ba64b1bfefc4d346c23942cf3
seoboom <seobm7996@gmail.com>
2019-10-17 16:51:38 +0900

    developed field component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,59 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import InfiniteCalendar from "react-infinite-calendar";
 import "react-infinite-calendar/styles.css"; // Make sure to import the default stylesheet
 
-import {fieldCodes} from "./constant/Yongsan";
+import { fieldCodes } from "./constant/Yongsan";
 import YongsanField from "./component/YongsanField/YongsanField";
 
 function App() {
-  const [today, setToday] = useState(new Date());
+	const [today, setToday] = useState(new Date());
 
-  const formatDate = date => {
-    var d = new Date(date),
-      month = "" + (d.getMonth() + 1),
-      day = "" + d.getDate(),
-      year = d.getFullYear();
+	const formatDate = date => {
+		let d = new Date(date),
+			month = "" + (d.getMonth() + 1),
+			day = "" + d.getDate(),
+			year = d.getFullYear();
 
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
+		if (month.length < 2) month = "0" + month;
+		if (day.length < 2) day = "0" + day;
 
-    return [year, month, day].join("-");
-  };
+		return [year, month, day].join("-");
+	};
 
-  return (
-    <Container>
-      <Row>
-        <Col xs={12} lg={12}>
-          <InfiniteCalendar
-            locale={{
-              headerFormat: "M월D일",
-              weekdays: ["일", "월", "화", "수", "목", "금", "토"]
-            }}
-            displayOptions={{
-              layout: "landscape"
-            }}
-            width={600}
-            height={350}
-            selected={today}
-            onSelect={setToday}
-          />
-        </Col>
-        <Col xs={12} lg={12}>
-          {fieldCodes.map((item, index) => (
-            <YongsanField
-              key={"field" + item}
-              date={formatDate(today)}
-              fieldCode={item}
-              fieldName={index + 1 + "구장"}
-            />
-          ))}
-        </Col>
-      </Row>
-    </Container>
-  );
+	const d = new Date();
+	const maxDate = new Date(d.getFullYear(), d.getMonth() + 2, 0);
+
+	return (
+		<Container style={{ paddingTop: "30px", paddingBottom: "50px" }}>
+			<Row>
+				<Col xs={12} lg={12}>
+					<InfiniteCalendar
+						locale={{
+							headerFormat: "M월D일",
+							weekdays: ["일", "월", "화", "수", "목", "금", "토"]
+						}}
+						displayOptions={{
+							layout: "landscape"
+						}}
+						width={600}
+						height={350}
+						selected={today}
+						onSelect={setToday}
+						maxDate={maxDate}
+					/>
+				</Col>
+				<Col xs={12} lg={12}>
+					{fieldCodes.map((item, index) => (
+						<YongsanField key={"field" + item} date={formatDate(today)} fieldCode={item} fieldName={index + 1 + "구장"} />
+					))}
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
--- src/component/YongsanField/YongsanField.jsx
+++ src/component/YongsanField/YongsanField.jsx
@@ -1,39 +1,54 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import qs from "qs";
 
 import "./YongsanField.css";
 
-import {Container, Row, Col} from "react-bootstrap";
+import { Spinner, Alert } from "react-bootstrap";
 
-function YongsanField({date, fieldCode, fieldName}) {
-  const [loading, setLoading] = useState(false);
+function YongsanField({ date, fieldCode, fieldName }) {
+	const [loading, setLoading] = useState(false);
+	const [feildList, setFieldList] = useState([]);
 
-  useEffect(() => {
-    setLoading(true);
+	useEffect(() => {
+		setLoading(true);
 
-    axios
-      .post(
-        "https://www.hdc-iparkmall.com/plugin/ret_StadData_New.asp",
-        qs.stringify({curdate: date, curstadcode: fieldCode}),
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          }
-        }
-      )
-      .then(response => {
-        console.log(response);
-        setLoading(false);
-      })
-      .catch(error => {});
-  }, [date]);
+		axios
+			.post("https://www.hdc-iparkmall.com/plugin/ret_StadData_New.asp", qs.stringify({ curdate: date, curstadcode: fieldCode }), {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded"
+				}
+			})
+			.then(response => {
+				console.log(response);
 
-  return (
-    <div className="field_block">
-      <h1 className="text-primary">{fieldName}</h1>
-    </div>
-  );
+				setFieldList(response.data.records);
+				setLoading(false);
+			})
+			.catch(error => {});
+	}, [date]);
+
+	return (
+		<div className="field_block">
+			<h1 className="text-secondary">{fieldName}</h1>
+			{loading ? (
+				<div className="spinner_wrapper">
+					<Spinner animation="border" variant="secondary" size={"sm"} />
+				</div>
+			) : (
+				<div>
+					{feildList.map((item, index) => {
+						if (item[4] == "")
+							return (
+								<Alert className="state_wrapper" key={"state" + fieldCode + index} variant={"success"}>
+									<strong>{item[3]}</strong>
+								</Alert>
+							);
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default YongsanField;
